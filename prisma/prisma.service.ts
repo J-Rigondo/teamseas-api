9664e037b8deb3d317b7c44274bc02f04f092c64
JJun <futuregoing46@gmail.com>
2022-06-29 18:34:22 +0900

    prsima query log 추가

--- prisma/prisma.service.ts
+++ prisma/prisma.service.ts
@@ -1,8 +1,39 @@
-import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
+import {
+  INestApplication,
+  Injectable,
+  Logger,
+  OnModuleInit,
+} from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { createPrismaQueryEventHandler } from 'prisma-query-log';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  private readonly logger = new Logger();
+
+  constructor() {
+    super({
+      log: [
+        { emit: 'event', level: 'query' },
+        { emit: 'stdout', level: 'info' },
+        { emit: 'stdout', level: 'warn' },
+        { emit: 'stdout', level: 'error' },
+      ],
+    });
+
+    this.$on(
+      'query' as any,
+      createPrismaQueryEventHandler({
+        logger: (query) => {
+          this.logger.verbose(query, 'PrismaClient');
+        },
+        format: false,
+        colorQuery: '\u001B[96m',
+        colorParameter: '\u001B[90m',
+      }),
+    );
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
