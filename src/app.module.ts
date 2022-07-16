c8c6b73b697f6fda4f269d42a3df558224a90f05
futuregoing <futuregoing@prismweaver.com>
2022-07-17 00:46:48 +0900

    실행 컨텍스트에 req, res 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,9 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
 import { join } from 'path';
-import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
-import { loggingMiddleware } from 'src/common/middleware/logging-prisma.middleware';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { RestModule } from './rest/rest.module';
 
 @Module({
   imports: [
@@ -16,24 +15,15 @@ import { PrismaModule } from 'src/prisma/prisma.module';
       plugins: [ApolloServerPluginLandingPageLocalDefault()],
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
       sortSchema: true,
+      context: ({ req, res }) => ({ req, res }),
+      // cors: {
+      //   origin: process.env.FRONT_END_URL,
+      //   credentials: true,
+      // },
     }),
-    // PrismaModule.forRoot({
-    //   isGlobal: true,
-    //   prismaServiceOptions: {
-    //     // middlewares: [loggingMiddleware()],
-    //     prismaOptions: {
-    //       log: [
-    //         { emit: 'stdout', level: 'query' },
-    //         { emit: 'stdout', level: 'info' },
-    //         { emit: 'stdout', level: 'warn' },
-    //         { emit: 'stdout', level: 'error' },
-    //       ],
-    //     },
-    //   },
-    // }),
-    // UsersModule,
     AuthModule,
     PrismaModule,
+    RestModule,
   ],
 })
 export class AppModule {}
