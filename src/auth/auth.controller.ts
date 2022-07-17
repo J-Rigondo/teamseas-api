3a954bbb0030388ad84b1df97ccd07497d03ab9a
futuregoing <futuregoing@prismweaver.com>
2022-07-17 19:04:04 +0900

    로컬 로그인 httpOnly 쿠키 세팅

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,37 @@
 import {
   Controller,
   Get,
+  Post,
   HttpStatus,
   Req,
   Res,
   UseGuards,
 } from '@nestjs/common';
 import { GoogleAuthGuard } from 'src/common/guard/google-auth.guard';
+import { AuthGuard } from '@nestjs/passport';
+import { AuthService } from 'src/auth/auth.service';
+import { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('/login')
+  @UseGuards(AuthGuard('local'))
+  async login(@Req() req, @Res({ passthrough: true }) res: Response) {
+    const { accessToken, refeshTokenInfo } = await this.authService.login(
+      req.user,
+    );
+
+    const { refreshToken, ...options } = refeshTokenInfo;
+    res.cookie('refresh', refreshToken, options);
+
+    return {
+      accessToken,
+      user: req.user,
+    };
+  }
+
   @Get('/google/login')
   @UseGuards(GoogleAuthGuard)
   googleLogin() {
@@ -19,6 +41,18 @@ export class AuthController {
   @Get('/google/callback')
   @UseGuards(GoogleAuthGuard)
   googleAuthRedirect(@Req() req, @Res() res) {
-    return res.redirect('http://localhost:3000/');
+    return res.redirect(process.env.FRONT_END_URL);
+  }
+
+  @Get('/kakao/login')
+  @UseGuards(AuthGuard('kakao'))
+  kakaoLogin() {
+    return HttpStatus.OK;
+  }
+
+  @Get('/kakao/callback')
+  @UseGuards(AuthGuard('kakao'))
+  kakaoAuthRedirect(@Req() req, @Res() res) {
+    return res.redirect(process.env.FRONT_END_URL);
   }
 }
