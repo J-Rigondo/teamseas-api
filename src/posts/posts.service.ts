0dd102f7eabb249c69ef127ca28684aef432d83f
JJun <futuregoing46@gmail.com>
2022-08-06 21:22:55 +0900

    aop

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -5,6 +5,29 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { PostResponse } from 'src/posts/dto/post-response';
 import { AsyncContextService } from 'src/async-context/async-context.service';
 
+function ExecuteLogAspect() {
+  return function (target: any, key: string, desc: PropertyDescriptor) {
+    console.log('=========================aop ExecuteLogAspect');
+    const originMethod = desc.value;
+
+    desc.value = async function (...args) {
+      const res = await originMethod.apply(this, args);
+      console.log('after execute method');
+      console.log('==================exit');
+
+      return res;
+    };
+  };
+}
+
+function ExecuteSendingAspect(
+  target: any,
+  key: string,
+  desc: PropertyDescriptor,
+) {
+  console.log('=========sending aspect ==============');
+}
+
 @Injectable()
 export class PostsService {
   constructor(
@@ -16,9 +39,13 @@ export class PostsService {
     return 'This action adds a new post';
   }
 
+  @ExecuteLogAspect()
+  @ExecuteSendingAspect
   async findAll(cursor: string) {
     const limit = 4;
     const cursorObj = cursor ? { id: +cursor } : undefined;
+
+    console.log('asyc', this.asyncContext.get());
 
     const posts = await this.prisma.post.findMany({
       take: limit,
@@ -28,8 +55,6 @@ export class PostsService {
         author: true,
       },
     });
-
-    console.log(posts);
 
     const response = new PostResponse();
     response.posts = posts;
